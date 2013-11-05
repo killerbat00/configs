c73a2f945b76cf22e0d076c95a6dfff3afbb66d0
brian morrow <bmorrow92@gmail.com>
2013-11-05 14:12:50 -0500

    Add dwm configuration as well as more program checking...

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+SCRIPT_DIR=`dirname "$(cd ${0%/*} && echo $PWD/${0##*/})"`
+
 if [ -z $1 ]; then
     echo "Please specify username..."
     exit 1
 else
     user=$1
 fi
-
+echo 'checking if zsh is installed...'
 zsh=$(which zsh)
 if [ -z $zsh ]; then
     echo 'no zsh...installing.'
     sudo yum install -y zsh
 fi
 
+echo 'checking to see if vim is installed...'
 vim=$(which vim)
 if [ -z $vim ]; then
     echo 'no vim...installing.'
@@ -20,7 +23,7 @@ if [ -z $vim ]; then
 fi
 
 echo "Changing shell to zsh..."
-chsh -s $zsh $user
+sudo chsh -s $zsh $user
 
 echo "Configuring zsh..."
 cp -R oh-my-zsh /home/$user/.oh-my-zsh
@@ -33,5 +36,12 @@ ln -s /home/$user/.vim/vimrc /home/$user/.vimrc
 echo "Configuring .xinitrc..."
 cp xinitrc /home/$user/.xinitrc
 cp Xresources /home/$user/.Xresources
+
+echo "Installing dwm..."
+sudo apt-get install dwm suckless-tools
+
+echo "Configuring dwm..."
+sudo apt-get source dwm
+cp $SCRIPT_DIR/config.h dwm-*/
 
 echo "Hey look at that, I'm done."
